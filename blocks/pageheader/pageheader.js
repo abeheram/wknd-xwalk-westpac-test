3c28f4883780995351fc4f6ba805499931df58a9
abeheram <74228542+abeheram@users.noreply.github.com>
2025-07-07 17:38:42 +1000

    Update pageheader.js

--- blocks/pageheader/pageheader.js
+++ blocks/pageheader/pageheader.js
@@ -1,10 +1,12 @@
 export default function decorate(block) {
-  const pageHeader = block.querySelector('.pageheader');  // Select the page-header div
+  const pageHeader = block.querySelector('.pageheader'); // Select the page-header div
   if (pageHeader) {
-     const blockquote = document.createElement('blockquote');
-      blockquote.innerHTML = `<strong>test content</strong>`;
-      pageHeader.appendChild(blockquote);
+    // Create the <blockquote> element
+    const blockquote = document.createElement('blockquote');
+    blockquote.innerHTML = `<strong>test content</strong>`;
+
+    // Append the <blockquote> to the pageHeader
+    pageHeader.insertAdjacentHTML('beforeend', blockquote.outerHTML);
   }
-   
 }
 
