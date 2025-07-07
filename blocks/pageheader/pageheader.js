8bae475906d076a1729f1d88d2559665c06645c0
abeheram <74228542+abeheram@users.noreply.github.com>
2025-07-07 18:24:47 +1000

    Update pageheader.js
    update logic

--- blocks/pageheader/pageheader.js
+++ blocks/pageheader/pageheader.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
    const innerDiv = block.querySelector("div > div"); // Select the inner <div> inside the "pageheader block"
     if (innerDiv) {
-       const htmlContent = "<strong>test1 content</strong>";
+       const htmlContent = "<strong>test12 content</strong>";
       // Append the HTML content
       innerDiv.insertAdjacentHTML("beforeend", htmlContent);
     }
