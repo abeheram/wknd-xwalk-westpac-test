8c907d412b2a4b644ca2f4a065675e8503654988
abeheram <74228542+abeheram@users.noreply.github.com>
2025-08-21 14:09:04 +1000

    Update product-cards.js

--- blocks/product-cards/product-cards.js
+++ blocks/product-cards/product-cards.js
@@ -1,9 +1,12 @@
 export default async function decorate(block) {
 
   const props = [...block.children];
-  const style = props[0].textContent.trim() || 'default';
-  const tag = props[1].textContent.trim();
- console.log('tag '+tag);
+  const style = props[0]?.textContent?.trim() || 'default';
+  const tag = props[1]?.textContent?.trim() || '';
+  block.innerHTML = `
+    <div class="filters" id="filters">${tag}</div>
+    <div class="grid" id="productGrid" data-style="${style}" data-tag="${tag}" class="${style}">${style}</div>
+  `;
  /*
  // Remove all inner content
   block.innerHTML = '';
