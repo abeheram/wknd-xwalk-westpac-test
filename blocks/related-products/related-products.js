790f60e97cf21965accbf9c2f3bf77274f9fece2
abeheram <74228542+abeheram@users.noreply.github.com>
2025-08-22 17:22:17 +1000

    Update related-products.js

--- blocks/related-products/related-products.js
+++ blocks/related-products/related-products.js
@@ -8,7 +8,7 @@ export default async function decorate(block) {
   // Add the filters and grid divs
   block.innerHTML = `
     <div class="related-container">
-      <h2 id="relatedHeading" style="margin-bottom:24px;"></h2>
+      <h2 id="relatedHeading" style="margin-bottom:24px;">${tag}</h2>
       <div class="grid" id="productGrid"></div>
     </div>
   `;
