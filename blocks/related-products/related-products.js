3aa7737232766f885f72e22b08a35db906cdd5c4
abeheram <74228542+abeheram@users.noreply.github.com>
2025-08-25 18:23:28 +1000

    Update related-products.js

--- blocks/related-products/related-products.js
+++ blocks/related-products/related-products.js
@@ -18,7 +18,7 @@ export default async function decorate(block) {
     let allFeatureTags = [];
     let allTags = [];
     // const filtersDiv = document.getElementById('filters');
-    const grid = document.getElementById('productGrid');
+    const grid = block.getElementById('productGrid');
 
     function formatTag(tag, type) {
       let prefix = '';
