e8f31e8b0f71a01c2dc87928188c1cb3427b2ce8
Amrita <amrita.behera@westpac.com.au>
2025-11-05 11:52:27 +1100

    Refactor head.html to remove module type from scripts-wdp.js and add inlinescript for data-wdp

--- scripts/scripts-wdp.js
+++ scripts/scripts-wdp.js
@@ -1,15 +1,8 @@
-require.config({
-    paths: {
-        'scriptjs': 'https://main--wknd-xwalk-westpac-test--abeheram.aem.live/scripts/scripts.js',
-        'franklinjs': 'https://main--wknd-xwalk-westpac-test--abeheram.aem.live/scripts/lib-franklin.js'
-    }
-});
-define([], function () {
+
+define(['https://main--wknd-xwalk-westpac-test--abeheram.aem.live/scripts/scripts.js'], function () {
     "use strict";
     function main(){
-        return  require(['scriptjs','franklinjs'],function(){
-            "use strict";
-       });
+       
     }
     return {
         wdp:main
