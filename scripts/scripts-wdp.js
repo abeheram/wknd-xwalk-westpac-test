8744ad4935ebcf195d0d50d4c84c2dd8ba3b80dc
Amrita <amrita.behera@westpac.com.au>
2025-11-05 13:26:46 +1100

    Refactor scripts-wdp.js to comment out AMD module definition and retain console log; add private key and public key files for SSH authentication

--- scripts/scripts-wdp.js
+++ scripts/scripts-wdp.js
@@ -1,10 +1,11 @@
 
-define(['https://main--wknd-xwalk-westpac-test--abeheram.aem.live/scripts/scripts.js'], function () {
-    "use strict";
-    function main(){
-       console.log("WDP script loaded");
-    }
-    return {
-        wdp:main
-    }
-});
+//define(['https://main--wknd-xwalk-westpac-test--abeheram.aem.live/scripts/scripts.js'], function () {
+   // "use strict";
+    //function main(){
+       //console.log("WDP script loaded");
+    //}
+    //return {
+        //wdp:main
+   // }
+//});
+console.log("WDP script loaded");
