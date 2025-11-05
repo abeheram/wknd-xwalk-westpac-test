b64506e3abda6cfd55d645ce68841598bb99ff32
Amrita <amrita.behera@westpac.com.au>
2025-11-05 13:28:58 +1100

    Restore WDP script functionality and add SSH authentication keys

--- scripts/scripts-wdp.js
+++ scripts/scripts-wdp.js
@@ -1,11 +1,11 @@
 
-//define(['https://main--wknd-xwalk-westpac-test--abeheram.aem.live/scripts/scripts.js'], function () {
-   // "use strict";
-    //function main(){
-       //console.log("WDP script loaded");
-    //}
-    //return {
-        //wdp:main
-   // }
-//});
-console.log("WDP script loaded");
+require(['https://main--wknd-xwalk-westpac-test--abeheram.aem.live/scripts/scripts.js'], function (a) {
+   "use strict";
+    function main(){
+       console.log("WDP script loaded first time");
+    }
+    return {
+        wdp:main
+    }
+});
+console.log("WDP script loaded second time");
