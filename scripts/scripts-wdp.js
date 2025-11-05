9af09d455cbf43a27c47ec09408882a004e14c0d
Amrita <amrita.behera@westpac.com.au>
2025-11-05 12:14:18 +1100

    Update m scripts-wdp.js and add inlinescript for data-wdp; log message in scripts-wdp.js

--- scripts/scripts-wdp.js
+++ scripts/scripts-wdp.js
@@ -2,7 +2,7 @@
 define(['https://main--wknd-xwalk-westpac-test--abeheram.aem.live/scripts/scripts.js'], function () {
     "use strict";
     function main(){
-       
+       console.log("WDP script loaded");
     }
     return {
         wdp:main
